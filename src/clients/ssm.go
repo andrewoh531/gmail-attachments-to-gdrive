8365da6e1b6752d1ea6549618789b725d1d4725b
Andrew Oh <andrewoh531@gmail.com>
2018-08-10 14:20:21 +1000

    Minor reshuffle of code

--- src/clients/ssm.go
+++ src/clients/ssm.go
@@ -5,16 +5,33 @@ import (
 	"sync"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/aws"
+	"fmt"
+	"os"
 )
 
 var ssmClient *ssm.SSM
 var once sync.Once
 
 // Retrieve a single instance of an SSM Client
-func GetSsmClient() *ssm.SSM {
+func getSsmClient() *ssm.SSM {
 	once.Do(func() {
 		sess := session.Must(session.NewSession())
 		ssmClient = ssm.New(sess, aws.NewConfig())
 	})
 	return ssmClient
+}
+
+func RetrieveFromParameterStore(key string) string {
+
+	output, err := getSsmClient().GetParameter(&ssm.GetParameterInput{
+		Name:           &key,
+		WithDecryption: aws.Bool(true),
+	})
+
+	if err != nil {
+		fmt.Printf("Error retrieving SSM (%s): %v", key, err)
+		os.Exit(1)
+	}
+
+	return *output.Parameter.Value
 }
--- src/main.go
+++ src/main.go
@@ -6,26 +6,11 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/ssm"
 
 	"personal/gmail-attachments-to-gdrive/src/clients"
 )
 
-func retrieveFromParameterStore(key string) string {
 
-	output, err := clients.GetSsmClient().GetParameter(&ssm.GetParameterInput{
-		Name:           &key,
-		WithDecryption: aws.Bool(true),
-	})
-
-	if err != nil {
-		fmt.Printf("Error retrieving SSM (%s): %v", key, err)
-		os.Exit(1)
-	}
-
-	return *output.Parameter.Value
-}
 
 func handler(request events.CloudWatchEvent) (events.APIGatewayProxyResponse, error) {
 
@@ -40,8 +25,8 @@ func handler(request events.CloudWatchEvent) (events.APIGatewayProxyResponse, er
 		4 - Send confirmation email for the google drive account of files uploaded
 	 */
 
-	gmailOAuthToken := retrieveFromParameterStore(os.Getenv("GMAIL_OAUTH_TOKEN"))
-	googleDriveOAuthToken := retrieveFromParameterStore(os.Getenv("GOOGLE_DRIVE_OAUTH_TOKEN"))
+	gmailOAuthToken := clients.RetrieveFromParameterStore(os.Getenv("GMAIL_OAUTH_TOKEN"))
+	googleDriveOAuthToken := clients.RetrieveFromParameterStore(os.Getenv("GOOGLE_DRIVE_OAUTH_TOKEN"))
 
 	return events.APIGatewayProxyResponse{
 		Body: fmt.Sprintf("GMAIL_OAUTH_TOKEN=%v, GMAIL_SEARCH_QUERY=%v, GOOGLE_DRIVE_OAUTH_TOKEN=%v, GOOGLE_DRIVE_UPLOAD_FOLDER=%v",
